e095c5ff6cd7586a839c646ce838130aa0606543
abdullahdurna <abdullahdurna41@gmail.com>
2025-12-03 13:22:57 +0300

    Object Tanımlama

--- Day1/helloworld.js
+++ Day1/helloworld.js
@@ -1,1 +1,13 @@
 console.log("Hello, World!");
+console.log(typeof "Hello, World!"); // string
+
+console.log(typeof null); // string
+
+let ad = "Apo";
+let soyad = "Loco";
+let ulke = "Mars";
+let sehir = "Ankara";
+let yas = 3000;
+
+let greeting = "Benim adım " + ad + " " + soyad + ". " + ulke + " ülkesinin " + sehir + " şehrinde yaşıyorum ve yaşım " + yas + ".";
+console.log(greeting);
