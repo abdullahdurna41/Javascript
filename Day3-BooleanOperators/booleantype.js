20ecb4b937fe75c159b0158822fa5ef53a4fcd99
abdullahdurna <abdullahdurna41@gmail.com>
2025-12-05 13:56:50 +0300

    Day3

---
+++ Day3-BooleanOperators/booleantype.js
@@ -0,0 +1,29 @@
+let isLightOn = true // ışık açık doğru
+
+let isHungry = false // aç yanlış
+let isMarried = true // evli doğru
+let truValue = 4 > 3    // true -- doğru
+let falseValue = 4 < 3  // false -- yanlış
+
+
+let isRaining = true
+isRaining
+  ? console.log('You need a rain coat.')
+  : console.log('No need for a rain coat.')
+isRaining = false
+
+isRaining
+  ? console.log('You need a rain coat.')
+  : console.log('No need for a rain coat.')
+
+  let number = 5
+number > 0
+  ? console.log(`${number} is a positive number`)
+  : console.log(`${number} is a negative number`)
+number = -5
+
+number > 0
+  ? console.log(`${number} is a positive number`)
+  : console.log(`${number} is a negative number`)
+
+  
---
+++ Day3-BooleanOperators/date.js
@@ -0,0 +1,9 @@
+const now = new Date()
+console.log(now) // Sat Jan 04 2020 00:56:41 GMT+0200 (Eastern European Standard Time)
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getDay());
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getSeconds());
